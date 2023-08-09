1756eaf3c81aec8ce67ce35f9aae615dbfb7a1cc
ouafcode <ouafaesaim@gmail.com>
2023-08-09 11:51:16 +0000

    task1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    last_dt = (((-1 * number) % 10)* -1)
+else:
+    last_dt = number % 10
+if last_dt > 5 :
+    print("Last digit of {} is {} and is greater than 5".format(number,last_dt))
+elif last_dt == 0:
+    print("Last digit of {} is {} and is 0".format(number,last_dt))
+elif last_dt < 6 and not 0:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number,last_dt))
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,3 +1,5 @@
 ### TASKS
 
 - Task1 : This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print whether the number stored in the variable number is positive or negative.
+
+- Task2 : This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable number.
