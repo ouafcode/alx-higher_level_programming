285de610dc2a04b3e2b136e57f3d5620849f70c3
ouafcode <ouafaesaim@gmail.com>
2023-08-11 19:31:51 +0000

    task3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv) - 1
-if n == 0:
-    print("{} arguments".format(n))
-elif n == 1:
-    print("{} argument:".format(n))
-    print("{} : {}".format(n, sys.argv[1]))
-else:
-    print("{} arguments:".format(n))
-    for x in range(1, n+1):
-        print("{} : {}".format(x, sys.argv[x]))
+if __name__ = "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("{} arguments".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+        print("{} : {}".format(n, sys.argv[1]))
+    else:
+        print("{} arguments:".format(n))
+        for x in range(1, n+1):
+            print("{} : {}".format(x, sys.argv[x]))
