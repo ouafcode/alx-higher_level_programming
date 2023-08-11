d8ad20ef7f3f15f8ae40dd85ff6a8d60e86e0069
ouafcode <ouafaesaim@gmail.com>
2023-08-11 22:54:06 +0000

    task6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, opr[sys.argv[2]](a,b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, opr[sys.argv[2]](a, b)))
