afa2c037ba4f4aef916943e2ece572ee9dbe06e4
ouafcode <ouafaesaim@gmail.com>
2023-08-14 21:25:35 +0000

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    temp = list(my_string)
-    for i in temp:
-        if i  == 'c' or i  == 'C':
-            temp.remove(i)
-    return(''.join(temp))
+    if my_string:
+        temp = list(my_string)
+        for i in temp:
+            if i == 'c' or i == 'C':
+                temp.remove(i)
+        return(''.join(temp))
