f57d5f5444ad87bb1bcaf5e4380f4c7608755de6
ouafcode <ouafaesaim@gmail.com>
2023-08-15 19:00:45 +0000

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = None
-    new = ((len(sentence), sentence[0]))
+        new = (0, None)
+    else:
+        new = ((len(sentence), sentence[0]))
     return new
