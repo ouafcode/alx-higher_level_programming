bd210de383d7a782e57ab892d2ccf62d4ff166c6
ouafcode <ouafaesaim@gmail.com>
2023-08-18 11:18:23 -0700

    task3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    new = []
     for x in set_1:
         for y in set_2:
             if x == y:
-                return (x)
+                new.append(x)
+    return (set(new))
