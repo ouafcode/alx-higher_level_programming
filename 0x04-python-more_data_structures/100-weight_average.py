f07397af0176a9f741f97339245413b0c99971db
ouafcode <ouafaesaim@gmail.com>
2023-08-19 14:08:44 -0700

    task13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return (0)
+    mult = 0
+    mult2 = 0
+    for i in range(len(my_list)):
+        for j in range(len(my_list[i]) - 1):
+            mult += my_list[i][j] * my_list[i][j+1]
+        mult2 += my_list[i][1]
+    avrg = mult / mult2
+    return (avrg)
