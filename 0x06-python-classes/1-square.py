de859d664c85732b7a5689deb2d9faa67b65b822
ouafcode <ouafaesaim@gmail.com>
2023-08-31 14:54:26 -0700

    task01

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,12 +2,10 @@
 """ Defining a class square """
 
 
-
 class Square:
     """ Defining a class square """
     def __init__(self, size):
         """ Initializing a square class
         Args: size: size of the square
          """
-
         self.__size = size
