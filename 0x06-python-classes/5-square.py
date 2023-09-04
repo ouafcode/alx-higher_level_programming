46442563636a512404228ae6cb383febdcd2c1b4
ouafcode <ouafaesaim@gmail.com>
2023-09-04 07:32:07 -0700

    task5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" Creasting a square class """
+""" Create class for defining square """
 
 
 class Square:
-    """ Defining a class square """
+    """ Create class for defining square """
     def __init__(self, size=0):
-        """ Initializing a square class
-        Args: size=0: size of the square
-         """
-        self.__size = size
+        """ Initialising attribute instance
+        Args: size: size of square
+        """
+        self.size = size
 
     @property
     def size(self):
-        """ Getting the size of the square """
+        """ Getting the size of square """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Setting the size of the square """
+        """ setting the size of square """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -25,13 +25,15 @@ class Square:
         self.__size = value
 
     def area(self):
-        """ Calculating the area of the square """
-        return (self.__size ** 2)
+        """ return current square area """
+        return self.__size ** 2
 
     def my_print(self):
-        """ Printing the square """
+        """ prints in stdout the square with the character # """
         if self.__size == 0:
-            print()
+            print("")
         else:
-            for i in range(self.__size):
-                print("#" * self.__size)
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end='')
+                print()
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -8,4 +8,6 @@
 
 - Task3: Write a class Square that defines a square by: (based on 2-square.py)
 
-- Task4: Write a class Square that defines a square by: (based on 2-square.py)
+- Task4: Write a class Square that defines a square by: (based on 3-square.py)
+
+- Task5: Write a class Square that defines a square by: (based on 4-square.py)
