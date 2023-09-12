3f5d77d2b5d54539e539cd0ad36fb165d93cea72
ouafcode <ouafaesaim@gmail.com>
2023-09-12 13:51:07 -0700

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Module 7-add_item.
-Adds all arguments to a Python list, and then save them to a file
+""" Module 9-add_item.
+Adds all arguments to a Python list,
+and then save them to a file
 """
 
 import sys
