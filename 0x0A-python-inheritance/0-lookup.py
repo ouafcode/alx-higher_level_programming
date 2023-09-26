27b9f789b281a87a84cad0bd0eeeeac9d583e7bf
ouafaesaim <ouafaesaim@gmail.com>
2023-09-26 12:51:00 +0200

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Creating a function Lookup"""
-
-
-def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object
-    """
-    return dir(obj)
--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Return true if the object is exactly an instance """
-
-
-def is_same_class(obj, a_class):
-    """
-    Return true if the object an  instance of a the specified class
-    """
-    return type(obj) == a_class
