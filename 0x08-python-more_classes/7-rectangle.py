20712f35e1d2d398fdc56b178943916100fb61ff
ouafcode <ouafaesaim@gmail.com>
2023-10-05 01:45:54 -0700

    task7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -48,7 +48,8 @@ class Rectangle:
     def __str__(self):
         if self.height == 0 or self.width == 0:
             return ("")
-        return (((str(self.print_symbol) * self.width)+"\n") * self.height)[:-1]
+        return (((str(self.print_symbol) * self.width)+"\n") *
+                self.height)[:-1]
 
     def __repr__(self):
         return ("Rectangle({}, {})".format(self.width, self.height))
