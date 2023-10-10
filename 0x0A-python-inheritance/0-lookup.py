c0130420704121785a374d5de88a400f4e10194e
ouafcode <ouafaesaim@gmail.com>
2023-10-10 07:37:15 -0700

    task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" fucntion to return attaribute and methods of object """
+
 
 def lookup(obj):
+    """ fucntion to return attaribute and methods of object """
     return (dir(obj))
