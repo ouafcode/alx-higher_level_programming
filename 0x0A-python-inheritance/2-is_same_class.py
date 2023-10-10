a54122d9c2cf80d8cf1cf7fdd8c1eb012dde95de
ouafcode <ouafaesaim@gmail.com>
2023-10-10 09:08:38 -0700

    task1

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" to check i a object is an instance of specified class """
+
+
+def is_same_class(obj, a_class):
+    """ to check i a object is an instance of specified class """
+    return type(obj) == a_class
