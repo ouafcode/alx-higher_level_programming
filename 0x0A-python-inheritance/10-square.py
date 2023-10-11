461559d3bf8df19312c2a0e094d4f774d1bc555b
ouafcode <ouafaesaim@gmail.com>
2023-10-11 10:39:22 -0700

    task10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A class that inherits from BaseGeometry """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class Square that inherits from Rectangle """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
