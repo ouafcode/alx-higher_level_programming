aa6aaeb1877d5240ee9675b526992c1a7df9acfa
ouafcode <ouafaesaim@gmail.com>
2023-10-11 05:18:58 -0700

    task4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,7 @@
 
 def inherits_from(obj, a_class):
     """ to check subclass """
-    return (issubclass(obj, a_class))
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return (True)
+    else:
+        return (False)
