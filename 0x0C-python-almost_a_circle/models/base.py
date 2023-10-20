858d2a7e023d03ee2d045413771cc5fc7994425c
ouafcode <ouafaesaim@gmail.com>
2023-10-20 14:50:36 -0700

    task16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Define the base class of project """
 import json
+import csv
+import os
 
 
 class Base():
@@ -21,3 +23,22 @@ class Base():
             return ("[]")
         else:
             return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ write json string of object in to file """
+        if (type(list_objs) != list and list_objs is not None
+           or not all(isinstance(x, cls) for x in list_objs)):
+            raise TypeError("list_objs must be list of objects")
+
+        name_file = cls.__name__+".csv"
+        with open(name_file, 'w') as f:
+            if list_objs is not None:
+                list_objs = [x.to_dictionary() for x in list_objs]
+                if cls.__name__ == 'Rectangle':
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == 'Square':
+                    fields = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(f, fieldnames=fields)
+                writer.writeheader()
+                writer.writerows(list_objs)
