1620449a59d7ab3d845c95465db99f1bdc42470b
ouafcode <ouafaesaim@gmail.com>
2023-10-20 11:45:46 -0700

    task15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Define the base class of project """
+import json
 
 
 class Base():
@@ -13,3 +14,10 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
