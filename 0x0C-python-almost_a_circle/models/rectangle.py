24901c48db183a3866cf257ac23f4320db637e9d
ouafcode <ouafaesaim@gmail.com>
2023-10-20 09:12:05 -0700

    task13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,4 +107,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """ return dict representation of class """
-        return ({'x': self.x, 'y': self.y, 'height': self.height, 'width': self.width})
+        return ({'x': self.x, 'y': self.y, 'id': self.id,
+                 'height': self.height, 'width': self.width})
