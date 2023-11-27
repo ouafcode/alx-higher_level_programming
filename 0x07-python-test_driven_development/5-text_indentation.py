ad3de756d786e3e4c64baa255a6505100e72f935
ouafcode <ouafaesaim@gmail.com>
2023-11-27 11:08:56 -0800

    task4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """ function that prints a text with 2 new lines
+        after each of these characters: ., ? and :
+    Args:
+        text (str): input text
+    Return:
+        text with 2 new lines
+    Raises:
+        TypeError: text must be a string
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    x = 0
+    while x < len(text) and text[x] == ' ':
+        x += 1
+    while x < len(text):
+        print(text[x], end="")
+        if text[x] == "\n" or text[x] in ".?:":
+            if text[x] in ".?:":
+                print("\n")
+            x += 1
+            while x < len(text) and text[x] == ' ':
+                x += 1
+            continue
+        x += 1
