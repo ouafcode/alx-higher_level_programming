de9b1f6a8750934fb66d97a69003f5b990913710
ouafcode <ouafaesaim@gmail.com>
2023-11-29 04:36:51 -0800

    task6

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -7,14 +7,14 @@ def matrix_mul(m_a, m_b):
     Args:
         m_a (matrice): input matrice
         m_b (matrice): input matrice
-    Return:
+    Returns:
         new matrice (m_a * m_b)
     Raises:
         TypeError: if m_a or m_b is not a list
         TypeError: if m_a or m_b is not a list of lists
         ValueError: if m_a or m_b is empty (it means: = [] or = [[]])
         TypeError: if one element of those list of lists is not
-                   an integer or a float
+        an integer or a float
         TypeError: if m_a or m_b is not a rectangle
         ValueError: If m_a and m_b can’t be multiplied
     """
