01d87afc6d201ea2e11c54285045700d58ca6795
ouafcode <ouafaesaim@gmail.com>
2023-12-04 06:49:29 -0800

    task1

---
+++ 0x0A-python-inheritance/1-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList()
+my_list.append("hello")
+my_list.append("ali")
+my_list.append("how")
+my_list.append("are")
+my_list.append("you")
+print(my_list)
+my_list.print_sorted()
+print(my_list)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+class MyList(list):
+    """ prints the list sorted """
+    def print_sorted(self):
+        print(sorted(self))
