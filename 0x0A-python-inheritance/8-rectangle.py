761d5f5e8f50bc893b013e30b73a0f245f0d70b4
ouafcode <ouafaesaim@gmail.com>
2023-12-04 11:56:48 -0800

    task8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,10 +13,11 @@ class BaseGeometry():
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """ define class Rectangle """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
-        self.__width = height
+        self.__height = height
