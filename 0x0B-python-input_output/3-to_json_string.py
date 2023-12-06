c9dd3da84629a68851a0bcc773f9387dcc0d988d
ouafcode <ouafaesaim@gmail.com>
2023-12-06 12:32:32 -0800

    task3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """ function to encoding object """
+import json
 
 
 def to_json_string(my_obj):
     """ function to encoding object """
-    return(json.dumps(my_obj))
+    return (json.dumps(my_obj))
