5841598aa0443d0037284e2435077499e253b3e2
ouafcode <ouafaesaim@gmail.com>
2023-12-06 12:58:26 -0800

    task5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ Encoding object to json in file """
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'w') as f:
         json.dump(my_obj, f)
