b3ed0666227f5acce0cb82562ce0153ff84cdb6e
ouafcode <ouafaesaim@gmail.com>
2023-12-06 05:11:26 -0800

    task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""  reads a text file (UTF8) and prints it to stdout """
+
+
+def read_file(filename=""):
+    """ reads a text file (UTF8) """
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
