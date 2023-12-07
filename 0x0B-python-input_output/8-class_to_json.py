01ad9e4df2bfa7e5325bd63d9f0fb32cfc087479
ouafcode <ouafaesaim@gmail.com>
2023-12-07 02:43:52 -0800

    task8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  returns the dictionary description """
+
+
+import json
+
+def class_to_json(obj):
+    """  returns the dictionary description """
+    return (obj.__dict__)
