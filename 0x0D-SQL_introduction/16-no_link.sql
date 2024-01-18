e0c480cfd90ef8f2c20347fffee18ea1d15b39ca
ouafcode <ouafaesaim@gmail.com>
2024-01-18 08:19:26 -0800

    task16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,8 +1,3 @@
 -- lists all records of the table second_table
-INSERT INTO second_table (score, name)
-VALUES
-(18, 'Aria'),
-(12, 'Aria');
-
 SELECT score, name FROM second_table
 ORDER BY score DESC;
