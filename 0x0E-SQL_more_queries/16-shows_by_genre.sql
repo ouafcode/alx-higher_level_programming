e3a3465184400c8055bc7541ed1bd606f04e356d
ouafcode <ouafaesaim@gmail.com>
2024-01-20 08:14:21 -0800

    task16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+--  lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT t.title, g.name
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN tv_genres AS g
+       ON g.`id` = s.`genre_id`
+
+ ORDER BY t.`title`, g.`name`
