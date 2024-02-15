8d89ec3b1c36b9dd8079a4dafe9bb0a66db394f9
ouafcode <ouafaesaim@gmail.com>
2024-02-15 15:56:42 -0800

    task8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let x = process.argv[2];
+if (isNaN(x) || x === undefined) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < x; i++){
+    console.log('X'.repeat(x));
+  }
+}
