8af4c2bf50c8df75b99766ec2315277463c4af81
ouafcode <ouafaesaim@gmail.com>
2024-02-16 08:34:30 -0800

    task10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const n = process.argv[2];
+function factorial(n) {
+  if (n <= 1 || isNaN(n)) {
+    return 1;
+} else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(n));
