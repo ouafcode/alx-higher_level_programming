34d91351a9ec9bb4b8022e1f01c3efb752863d3c
ouafcode <ouafaesaim@gmail.com>
2024-02-23 13:46:47 -0800

    task14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python file contain city class """
+
+from model_state import Base, State
+from enum import unique
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """ City class """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
