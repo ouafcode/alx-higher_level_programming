9a9650f9692569b3c1a00503e8b3f1aeb67ebb1e
ouafcode <ouafaesaim@gmail.com>
2024-02-23 05:41:34 -0800

    task6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """Python file contain state class """
 
-import MySQLdb
-import sys
+
 from enum import unique
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
-db = MySQLdb.connect(host='localhost', port=3306,
-                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
+Base = declarative_base()
 
 Base = declarative_base()
 
