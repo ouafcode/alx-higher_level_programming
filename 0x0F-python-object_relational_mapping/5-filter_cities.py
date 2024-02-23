aba408a9f0cecb0964bc607eef3f96ddf4d7f67f
ouafcode <ouafaesaim@gmail.com>
2024-02-23 04:16:34 -0800

    task5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" list all states in database """
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities\
+                 LEFT JOIN states\
+                 ON cities.state_id = states.id\
+                 WHERE states.name = %s\
+                 ORDER BY cities.id ASC",(sys.argv[4],))
+    query_row = cur.fetchall()
+    print(", ".join([row[0] for row in query_row]))
+    cur.close()
+    db.close()
